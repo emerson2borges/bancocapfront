b743900bc66a8deb5705e9f9b35c8fb9fb1a9970
Emerson Borges <emerson2borges@gmail.com>
2021-04-18 16:54:15 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="" id="app"></div>
+    <div class='container grid-xs py-2'>
+      <img class="img-responsive img-logo" src="@/assets/banco.png" alt="imagem banco">
+      <form @submit.prevent="sacar(asacar)">
+        <div class="input-group">
+          <input type="number" v-model="asacar" class="form-input" placeholder="Valor a sacar">
+          <button class="btn- btn-primary input-group-btn">SACAR</button>
+        </div>
+      </form>
+      <br />
+      <form @submit.prevent="depositar(adepositar)">
+        <div class="input-group flex-centered">
+          <input type="number" v-model="adepositar" class="form-input" placeholder="Valor a depositar">
+          <button class="btn- btn-primary input-group-btn">DEPOSITAR</button>
+        </div>
+      </form>
+      <div>
+        <br />
+        <p>
+          <label>Seu saldo é de: R$ </label><label>{{ saldo }}</label>
+        </p>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return { saldo: 200, asacar: 0, adepositar: 0 }
+  },
+  methods: {
+    depositar(valor) {
+      this.saldo = this.saldo + parseInt(valor);
+      this.adepositar.valueOf='0';
+    },
+    sacar(valor) {
+      if(this.saldo > parseInt(valor)) {
+        this.saldo -= parseInt(valor);
+      } else {
+        alert('Saque não realizado.')
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img.logo {
+  max-width: 200px;
+  margin: 0 auto;
+  align-content: center;
 }
 </style>
+
